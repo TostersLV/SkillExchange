0790bb0c7643eb0195ae914b346f1a99b256bb15
TostersLV <rpeizums@gmail.com>
2026-09-08 23:12:36 +0300

    Fixed code styles with Pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,17 +10,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
-
 
 #[Fillable(['username', 'bio', 'email', 'password', 'profile_picture'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable
 {
-
-
-
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -7,7 +7,6 @@ use App\Models\User;
 
 class PostPolicy
 {
-    
     public function update(User $user, Post $post): bool
     {
         return $user->id === $post->user_id;
