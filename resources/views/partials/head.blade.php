f6df28edeb0e7f9d2700ed6f7a1d235fcf5b624f
TostersLV <rpeizums@gmail.com>
2026-09-24 10:46:42 +0300

    Fixed style white flash

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -12,4 +12,20 @@
 @fonts
 
 @fluxAppearance
+{{-- Dark background from the very first frame of a page load (before the stylesheet arrives). --}}
+<style>
+    html.dark { background-color: #0e1117; }
+</style>
+{{-- Remember the current theme in a cookie so the server renders <html class="dark"> on the next page (no white flash). --}}
+<script>
+    (() => {
+        const html = document.documentElement;
+        const saveAppearance = () => {
+            document.cookie = 'appearance=' + (html.classList.contains('dark') ? 'dark' : 'light') + ';path=/;max-age=31536000;SameSite=Lax';
+        };
+
+        saveAppearance();
+        new MutationObserver(saveAppearance).observe(html, { attributes: true, attributeFilter: ['class'] });
+    })();
+</script>
 @vite(['resources/css/app.css', 'resources/js/app.js'])
